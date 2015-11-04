8c4e53977e3e0e0b6d5c6bf0cf45f6ef556ceb46
Axel Wallau <axel.wallau.github@dcs-fuerth.de>
2015-11-04 09:42:16 +0100

    tmp

---
+++ js/flexbox.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  var currentSection = 0;
+  var nextSection;
+  $('body').on('keydown', function (e) {
+    if (e.keyCode == 40) {
+      currentSection += 1;
+      nextSection = $('section').eq(currentSection);
+      if (nextSection[0] == undefined) {
+        currentSection = currentSection - 1;
+        nextSection = $('section').eq(currentSection);
+      }
+      $('body').scrollTo(nextSection, 300);
+    }
+
+    if (e.keyCode == 38) {
+      e.preventDefault();
+      currentSection = currentSection - 1 < 0 ? 0 : currentSection - 1;
+      nextSection = $('section').eq(currentSection);
+      $('body').scrollTo(nextSection, 300);
+    }
+  })
+});
