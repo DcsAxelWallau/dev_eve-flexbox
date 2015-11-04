a6883da33c2cfaba43dafd2568a271a75db2d406
Axel Wallau <axel.wallau.github@dcs-fuerth.de>
2015-11-04 13:16:50 +0100

    anim

--- js/flexbox.js
+++ js/flexbox.js
@@ -1,21 +1,26 @@
 $(document).ready(function () {
-  var nextSection, currentSection = 0;
+  var currentSection, nextSection, currentIndex = 0;
   $('body').on('keydown', function (e) {
 
-    if (e.keyCode == 40) {
-      currentSection += 1;
-      nextSection = $('section').eq(currentSection);
-      if (nextSection[0] == undefined) {
-        currentSection = currentSection - 1;
-        nextSection = $('section').eq(currentSection);
-      }
-      $('body').scrollTo(nextSection, 300);
-    }
-
-    if (e.keyCode == 38) {
+    if (e.keyCode == 40 || e.keyCode == 38) {
       e.preventDefault();
-      currentSection = currentSection - 1 < 0 ? 0 : currentSection - 1;
-      nextSection = $('section').eq(currentSection);
+      currentSection = $('section').eq(currentIndex);
+      currentSection.removeClass('active');
+
+      if (e.keyCode == 40) {
+        currentIndex += 1;
+        nextSection = $('section').eq(currentIndex);
+        if (nextSection[0] == undefined) {
+          currentIndex = currentIndex - 1;
+          nextSection = $('section').eq(currentIndex);
+        }
+
+      }
+      if (e.keyCode == 38) {
+        currentIndex = currentIndex - 1 < 0 ? 0 : currentIndex - 1;
+        nextSection = $('section').eq(currentIndex);
+      }
+      nextSection.addClass('active');
       $('body').scrollTo(nextSection, 300);
     }
 
